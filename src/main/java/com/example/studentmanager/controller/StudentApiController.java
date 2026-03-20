f25b40d19173e4f4cb541504f0157c332a021818
Lê Trung Kiên <kien102201@donga.edu.vn>
2026-03-20 14:32:22 +0700

    studentmanager

---
+++ src/main/java/com/example/studentmanager/controller/StudentApiController.java
@@ -0,0 +1,58 @@
+package com.example.studentmanager.controller;
+
+import com.example.studentmanager.model.Student;
+import com.example.studentmanager.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentApiController {
+
+    @Autowired
+    private StudentRepository repo;
+
+    @GetMapping
+    public List<Student> getAll() {
+        return repo.findAll();
+    }
+
+    @PostMapping
+    public Student add(@RequestBody Student s) {
+        return repo.save(s);
+    }
+
+    // 2: Xóa
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        repo.deleteById(id);
+        return "Deleted!";
+    }
+
+    // 3: Search
+    @PostMapping("/search")
+    public List<Student> search(@RequestBody Student s) {
+        return repo.findByNameContainingIgnoreCase(s.getName());
+    }
+
+    // 4: Get by ID
+    @GetMapping("/{id}")
+    public Student getById(@PathVariable Integer id) {
+        Optional<Student> s = repo.findById(id);
+        return s.orElse(null);
+    }
+
+    // 6: Update
+    @PostMapping("/update/{id}")
+    public Student update(@PathVariable Integer id, @RequestBody Student newS) {
+        return repo.findById(id).map(s -> {
+            s.setName(newS.getName());
+            s.setEmail(newS.getEmail());
+            s.setAge(newS.getAge());
+            return repo.save(s);
+        }).orElse(null);
+    }
+}
